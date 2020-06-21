1c0892960c2e057e7a68b307a9578ef645c8ae56
bartje1974 <bart@cs-hosting.nl>
2020-06-21 08:01:27 +0200

    renamed view databases

--- app/Http/Controllers/DatabasesController.php
+++ app/Http/Controllers/DatabasesController.php
@@ -14,7 +14,7 @@ class DatabasesController extends Controller
      */
     public function index()
     {
-        return view('database.index');
+        return view('databases.index');
     }
 
     /**
